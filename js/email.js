4b840159ba71fbe0758b216479f3bbdca76cc835
cortesnicolas <cortesnicolas006@gmail.com>
2021-04-08 22:24:21 -0400

    Integracion del Email
    Integracion de email y arreglo del carrusel

--- js/email.js
+++ js/email.js
@@ -1,28 +1,46 @@
 $("#enviarFormulario").on("click", function (event) {
-    event.preventDefault();
-    // resto de tu codigo
-    var nombre = document.getElementById("nombre").value;
-    var email = document.getElementById("email").value;
-    var asunto = document.getElementById("asunto").value;
-    var mensaje = document.getElementById("mensaje").value;
+  event.preventDefault();
+  // resto de tu codigo
+  var nombre = document.getElementById("nombre").value;
+  var email = document.getElementById("email").value;
+  var asunto = document.getElementById("asunto").value;
+  var mensaje = document.getElementById("mensaje").value;
+  var fono = document.getElementById("fono").value;
 
-    fetch("http://127.0.0.1:3000/api/email", {
-        headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify({ 
-            "nombre": nombre, 
-            "email": email,
-            "asunto": asunto,
-            "mensaje": mensaje
-        }),
-    })
+  fetch("http://127.0.0.1:3000/api/email", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      nombre: nombre,
+      email: email,
+      asunto: asunto,
+      mensaje: mensaje,
+      fono: fono,
+    }),
+  })
     .then(function (res) {
-        console.log(res);
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Mensaje Enviado",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      vaciaCampos();
+      console.log(res);
     })
     .catch(function (res) {
-        console.log(res);
+      console.log(res);
     });
 });
+
+function vaciaCampos() {
+  $("#nombre").val("");
+  $("#email").val("");
+  $("#asunto").val("");
+  $("#mensaje").val("");
+  $("#fono").val("");
+}
--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+var slideIndex = 1;
+var myTimer;
+var slideshowContainer;
+
 $(document).ready(function () {
   (function ($) {
     $(function () {
@@ -47,12 +51,65 @@ $(document).ready(function () {
     });
   })(jQuery);
 
-  $(".slider").bxSlider({
-    auto: true,
-    autoControls: true,
-    stopAutoOnClick: true,
-    pager: false,
-    responsive: true,
-    slideWidth: 2000
-  });
+  showSlides(slideIndex);
+  myTimer = setInterval(function () {
+    plusSlides(1);
+  }, 4000);
 });
+
+// NEXT AND PREVIOUS CONTROL
+function plusSlides(n) {
+  clearInterval(myTimer);
+  if (n < 0) {
+    showSlides((slideIndex -= 1));
+  } else {
+    showSlides((slideIndex += 1));
+  }
+
+  //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+
+  if (n === -1) {
+    myTimer = setInterval(function () {
+      plusSlides(n + 2);
+    }, 4000);
+  } else {
+    myTimer = setInterval(function () {
+      plusSlides(n + 1);
+    }, 4000);
+  }
+}
+
+//Controls the current slide and resets interval if needed
+function currentSlide(n) {
+  clearInterval(myTimer);
+  myTimer = setInterval(function () {
+    plusSlides(n + 1);
+  }, 4000);
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+pause = () => {
+  clearInterval(myTimer);
+};
+
+resume = () => {
+  clearInterval(myTimer);
+  myTimer = setInterval(function () {
+    plusSlides(slideIndex);
+  }, 4000);
+};
